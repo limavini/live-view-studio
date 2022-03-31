e9fcc94bb0a1345545a8ee444524de2cf4475746
limavini <viniciusalmeidait@gmail.com>
2022-03-31 17:07:49 -0300

    add boats live module

---
+++ lib/live_view_studio_web/live/boats_live.ex
@@ -0,0 +1,53 @@
+defmodule LiveViewStudioWeb.BoatsLive do
+  use LiveViewStudioWeb, :live_view
+  alias LiveViewStudio.Boats
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, boats: Boats.list_boats(), type: "")
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H(
+      <h1>Daily Boat Rentals</h1>
+    <div id="filter">
+      <form phx-change="filter">
+        <div class="filters">
+          <select name="type">
+          <%= options_for_select(type_options(), @type) %>
+          </select>
+        </div>
+      </form>
+      <div class="boats">
+        <%= for boat <- @boats do %>
+          <div class="card">
+            <img src={boat.image}>
+            <div class="content">
+              <div class="model">
+                <%= boat.model %>
+              </div>
+              <div class="details">
+                <span class="price">
+                  <%= boat.price %>
+                </span>
+                <span class="type">
+                  <%= boat.type %>
+                </span>
+              </div>
+            </div>
+          </div>
+        <% end %>
+      </div>
+    </div>
+    )
+  end
+
+  defp type_options do
+    [
+      "All Types": "",
+      Fishing: "fishing",
+      Sporting: "sporting",
+      Sailing: "sailing"
+    ]
+  end
+end
